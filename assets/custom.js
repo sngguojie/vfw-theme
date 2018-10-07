e797a678c681dd166721bd78110c0c6be211f0b0
sngguojie <sngguojie@gmail.com>
2018-10-08 03:04:49 +0800

    Add Semantic UI Add Animated Search Bar Add Card for collection

---
+++ assets/custom.js
@@ -0,0 +1,18 @@
+$( "#home-featured" ).prepend( "<div class=\"ui card\">\n" +
+  "  <div class=\"ui slide masked reveal image\">\n" +
+  "    <img src=\"https://semantic-ui.com/images/avatar/large/jenny.jpg\" class=\"visible content\">\n" +
+  "    <img src=\"https://semantic-ui.com/images/avatar/large/elliot.jpg\" class=\"hidden content\">\n" +
+  "  </div>\n" +
+  "  <div class=\"content\">\n" +
+  "    <a class=\"header\">Team Fu &amp; Hess</a>\n" +
+  "    <div class=\"meta\">\n" +
+  "      <span class=\"date\">Created in Sep 2014</span>\n" +
+  "    </div>\n" +
+  "  </div>\n" +
+  "  <div class=\"extra content\">\n" +
+  "    <a>\n" +
+  "      <i class=\"users icon\"></i>\n" +
+  "      2 Members\n" +
+  "    </a>\n" +
+  "  </div>\n" +
+  "</div>" );
